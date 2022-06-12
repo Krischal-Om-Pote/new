8a9043dd4a8e1cce48e7e7893bad03ff2f8c1f2a
Krischal-Om-Pote <103623800+Krischal-Om-Pote@users.noreply.github.com>
2022-06-12 09:49:54 +0545

    initial commit

---
+++ ecommerce_krischal/product_module/admin.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+
+
+# Register your models here.
+from .models import Brand, Category, Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["image_tag","name", "price", "brand", "category",]
+    search_fields = ["name", "price", "brand__name", "category__name",]
+    list_filter = ["brand","category",]
+    # readonly_fields = ["quantity",]
+ 
+    class Meta:
+        model = Product
+admin.site.register(Product, ProductAdmin)
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ["name",]
+    search_fields = ["name",]
+    # list_filter = ["brand","category",]
+    # readonly_fields = ["quantity",]
+    
+    class Meta:
+        model = Brand
+admin.site.register(Brand, BrandAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name",]
+    search_fields = ["name",]
+    # list_filter = ["brand","category",]
+    # readonly_fields = ["quantity",]
+    
+    class Meta:
+        model = Category
+admin.site.register(Category, CategoryAdmin)
